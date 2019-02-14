e92e376e6d4d093272967c05154d7bf32c6ce6cd
xulingyu@100tal.com <xulingyu@100tal.com>
2019-02-14 19:12:28 +0800

    shop

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,44 @@
 <template>
- <div>我的</div>
+ <div><van-tabs>
+    <van-tab title="登录">
+ <van-cell-group>
+  <van-field  required  clearable v-model="loginUsername" placeholder="请输入用户名" label="用户名"/>
+  <van-field  required   clearable type="password" v-model="loginPassword" placeholder="请输入密码" label="密码"/>
+</van-cell-group>
+<div>
+    <van-button type="primary" size="large">登录</van-button>
+</div>
+
+    </van-tab>
+    <van-tab title="注册">
+        <van-cell-group>
+  <van-field  required  clearable v-model="registerUsername" placeholder="请输入用户名" label="用户名"/>
+  <van-field  required   clearable type="password" v-model="registerPassword" placeholder="请输入密码" label="密码"/>
+</van-cell-group>
+<div>
+    <van-button type="primary" size="large" @click="registHander">注册</van-button>
+</div>
+    </van-tab>
+</van-tabs></div>
 </template>
 
 <script>
+import axios from 'axios';
+export default{
+data() {
+    return {
+       loginUsername:"",
+       loginPassword:"",
+       registerUsername:"",
+       registerPassword:""
+
+    }
+},
+methods:{
+registHander(){
+    axios().then().catch()
+}
+}
+}
 
 </script>
