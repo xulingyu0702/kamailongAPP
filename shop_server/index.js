b3a852f79dfd937994b59ed55b73b76d5d371c5e
xulingyu@100tal.com <xulingyu@100tal.com>
2019-02-21 18:25:13 +0800

    shop

--- shop_server/index.js
+++ shop_server/index.js
@@ -12,8 +12,10 @@ app.use(bodyParser());
 //加载路由
 const Router = require('koa-router');
 let user = require('./controller/user.js');
+let product =require('./controller/product.js');
 let router = new Router();
 router.use('/user', user.routes());
+router.use('/product',product.routes());
 
 app.use(router.routes());
 app.use(router.allowedMethods());
